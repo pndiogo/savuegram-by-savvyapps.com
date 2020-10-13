7e9893c33c9a2f978f3416b9e53f15883e2ab7a7
Paulo Diogo <pndiogo@gmail.com>
2020-10-13 11:48:20 +0100

    finished project

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,46 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app">
+        <SiteNav v-if="showNav"></SiteNav>
+        <router-view />
     </div>
-    <router-view/>
-  </div>
 </template>
+
+<script>
+import { mapState } from 'vuex';
+import SiteNav from '@/components/SiteNav';
+
+export default {
+    components: {
+        SiteNav,
+    },
+    computed: {
+        ...mapState(['userProfile']),
+        showNav() {
+            return Object.keys(this.userProfile).length > 1;
+        },
+    },
+};
+</script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
 }
 
 #nav {
-  padding: 30px;
+    padding: 30px;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+    a {
+        font-weight: bold;
+        color: #2c3e50;
 
-    &.router-link-exact-active {
-      color: #42b983;
+        &.router-link-exact-active {
+            color: #42b983;
+        }
     }
-  }
 }
 </style>
